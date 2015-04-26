8cb56e262f9c6d186abe5561807f2f8fe339c939
ramaroberto <bertoski@gmail.com>
2015-04-26 17:49:48 -0300

    Added ghost key support.

--- ghostAgents.py
+++ ghostAgents.py
@@ -79,3 +79,80 @@ class DirectionalGhost( GhostAgent ):
         for a in legalActions: dist[a] += ( 1-bestProb ) / len(legalActions)
         dist.normalize()
         return dist
+	
+class KeyboardGhost( GhostAgent ):
+        """
+        A ghost controlled by the keyboard.
+        """
+        WEST_KEY  = ''
+        EAST_KEY  = ''
+        NORTH_KEY = ''
+        SOUTH_KEY = ''
+        STOP_KEY = ''
+
+        def __init__( self, index = 0 ):
+
+            self.lastMove = Directions.STOP
+            self.index = index
+            self.keys = []
+			
+			# Binding of keys
+			# Up to 4 players
+            if index == 1:
+				self.NORTH_KEY = 'Up'
+                self.WEST_KEY  = 'Left'
+                self.SOUTH_KEY = 'Down'
+                self.EAST_KEY  = 'Right'
+                self.STOP_KEY = 'Space'
+                
+            if index == 2:
+				self.NORTH_KEY = 'w'
+                self.WEST_KEY  = 'a'
+                self.SOUTH_KEY = 's'
+                self.EAST_KEY  = 'd'
+                self.STOP_KEY = 'q'
+                
+			if index == 3:
+				self.NORTH_KEY = 't'
+                self.WEST_KEY  = 'f'
+                self.SOUTH_KEY = 'g'
+                self.EAST_KEY  = 'h'
+                self.STOP_KEY = 'r'
+			
+			if index == 4:
+				self.NORTH_KEY = 'i'
+                self.WEST_KEY  = 'j'
+                self.SOUTH_KEY = 'k'
+                self.EAST_KEY  = 'l'
+                self.STOP_KEY = 'u'
+
+        def getAction( self, state):
+            from graphicsUtils import keys_waiting
+            from graphicsUtils import keys_pressed
+            keys = keys_waiting() + keys_pressed()
+            if keys != []:
+                self.keys = keys
+
+            legal = state.getLegalActions(self.index)
+            move = self.getMove(legal)
+
+            if move == Directions.STOP:
+                # Try to move in the same direction as before
+                if self.lastMove in legal:
+                    move = self.lastMove
+
+            if (self.STOP_KEY in self.keys) and Directions.STOP in legal: move = Directions.STOP
+
+            if move not in legal:
+                move = random.choice(legal)
+
+            self.lastMove = move
+            return move
+
+        def getMove(self, legal):
+            move = Directions.STOP
+            if   (self.WEST_KEY in self.keys) and Directions.WEST in legal:  move = Directions.WEST
+            if   (self.EAST_KEY in self.keys) and Directions.EAST in legal: move = Directions.EAST
+            if   (self.NORTH_KEY in self.keys) and Directions.NORTH in legal:   move = Directions.NORTH
+            if   (self.SOUTH_KEY in self.keys) and Directions.SOUTH in legal: move = Directions.SOUTH
+            return move
