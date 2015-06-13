c0cc9d4b309f5b7d56311a83650501cc131e1208
ramaroberto <bertoski@gmail.com>
2015-06-13 13:40:40 -0300

    Added zoom parameter

--- run_pacman.sh
+++ run_pacman.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python pacman.py -p ApproximateQAgent -a extractor=SimpleExtractor -l mediumClassic --ghosts KeyboardGhost --numghosts 2 --gameMenu
+python pacman.py -p ApproximateQAgent -a extractor=SimpleExtractor -l mediumClassic --ghosts DirectionalGhost --numghosts 2 --gameMenu --zoom 1
