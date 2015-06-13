d6f59073701283231799fa38e5d014ca48dea99a
ramaroberto <bertoski@gmail.com>
2015-06-13 13:46:06 -0300

    Some training added

--- run_pacman_frame.sh
+++ run_pacman_frame.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python pacman.py -p ApproximateQAgent -a extractor=SimpleExtractor -l mediumClassic --ghosts DirectionalGhost --numghosts 2 --frameTime -1
+python pacman.py -p ApproximateQAgent -a extractor=SimpleExtractor -l mediumClassic --ghosts DirectionalGhost --numghosts 2 -x 10 -n 1010  --frameTime -1
